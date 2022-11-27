a909c2149199390ed784e30b1dfbef7706f38042
Vinay Joshi <joshivinay085@gmail.com>
2022-11-27 23:55:10 +0530

    dataTypes

---
+++ class1-dataType-CipherSchools.py
@@ -0,0 +1,32 @@
+print("Hello World")
+# Every line executed in python is either a:
+# 1. Statement
+# 2. Expression
+print("Vinay Joshi")
+print(100+200)
+a = 'a'
+print(type(a))
+# 4 very basic datatypes are:
+# 1.integers
+# 2.float
+# 3.string
+# 4.complex
+print(isinstance('a', str))
+print(isinstance('a', int))
+print(isinstance('a', object))
+a = 255
+print(id(a))
+print(hex(id(a)))
+b = 200
+print(id(b))
+c = 300
+print(id(c))
+print(hex(id(c)))
+a = 'aaaaa'
+print(id(a))
+b = 'aaaaa'
+print(id(b))
+a = 3246848423135468421321878486132154687132123168
+print(a+1)
+
+
