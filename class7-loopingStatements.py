a27ecec60320d5cc8afeac2fd3d731963fefc451
Vinay Kumar Joshi <joshivinay085@gmail.com>
2022-12-01 22:21:05 +0530

    Looping Statements

---
+++ class7-loopingStatements.py
@@ -0,0 +1,41 @@
+a = 1
+while a < 10:
+    print(a)
+    a += 1
+name = "Vinay"
+name.__iter__
+print(type(name))
+for c in name:
+    print(c)
+    print(type(c))
+for i in range(5):
+    print(i)
+a = range(5)
+print(a)
+for i in range(5):
+    if i == 3:
+        continue
+    print(i)
+for i in range(5):
+    print(i)
+    if i == 3:
+        continue
+for i in [0, 1, 2, 3, 4]:
+    print(i)
+    i = 100
+    print(i)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
