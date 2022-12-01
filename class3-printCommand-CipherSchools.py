2ca044dd14c80f75d700b666ddc3b2b7a4294705
Vinay Kumar Joshi <joshivinay085@gmail.com>
2022-12-01 20:09:21 +0530

    Print Command

--- class3-printCommand-CipherSchools.py
+++ class3-printCommand-CipherSchools.py
@@ -1,7 +1,7 @@
 print("Vinay Joshi")
 print(1)
 # print can accept infinite number of elements.
-print(1, 2, 3, 4, 5, 6, sep="_")
+print(1, 2, 3, 4, 5, 6, 7 sep="_")
 print(1, 2, 3, "vinay", 3.4, 1 + 5j, sep=",,")
 print("Hello")
 print("World !")
