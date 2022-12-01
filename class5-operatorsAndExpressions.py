ce62033d50865ddac0b8044cfa07931779408a03
Vinay Kumar Joshi <joshivinay085@gmail.com>
2022-12-01 21:15:28 +0530

    Operators and Expressions

---
+++ class5-operatorsAndExpressions.py
@@ -0,0 +1,50 @@
+# Operators
+print(5+5)
+print(10-5)
+print(10/3)
+print(isinstance(10.0, int))
+print(10//3)
+print(10%3)
+print(2*3)
+print(2**3)
+print("vinay" + " joshi")
+print("abc" * 5)
+# Comparison Operators
+print(1 == 2)
+print(1 != 2)
+print(1<2)
+print(2>3)
+print("ab" < "z")
+print("ab" < "AB")
+# Logical Operators
+print(True and False)
+print(True or False)
+print(True and 1)
+print(1 and 0)
+print(1 and 5)
+print(isinstance(True, int))
+print(type(True))
+print(bool(1))
+print(bool(0))
+print("vinay" and 5)
+
+#  Short Curcuiting
+# a or b = a (if a is truthy)
+# a or b = b (if a is falsy)
+# a and b = b (if a is truthy)
+# a and b = a (if a is falsy)
+
+# true or b = true
+# false or b = b
+# true and b = b
+# false and b = false
+print("" and 6)
+print(1.6 or "")
+print("" or 2.5)
+print(bool(""))
+print("" and 0)
+
+
+
+
+
