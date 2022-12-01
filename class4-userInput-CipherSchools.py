b7a5168c6a3ad1fa4c3461437d736fd642e7c777
Vinay Joshi <joshivinay085@gmail.com>
2022-12-01 20:05:16 +0530

    User Input and Type Conversion

---
+++ class4-userInput-CipherSchools.py
@@ -0,0 +1,15 @@
+a = input()
+print(a)
+print(type(a))
+print(bin(65))
+
+# Typecoercion--> automatic conversion by interpreter
+b = 15
+print(str(b))
+print(float(b))
+a = 65
+print(isinstance(a, object))
+a = "A"
+print(isinstance(a, object))
+
+
