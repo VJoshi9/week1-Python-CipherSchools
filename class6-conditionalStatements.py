9d124146d6cb9631b3e936a4296b0438ba6ed0d3
Vinay Kumar Joshi <joshivinay085@gmail.com>
2022-12-01 21:52:40 +0530

    Conditional Statements

---
+++ class6-conditionalStatements.py
@@ -0,0 +1,55 @@
+
+# 1 indent = 2 spaces, 4 spaces, tab, 8 spaces
+a = False
+if a:
+    print("the value is true")
+print("end")
+a = False
+if a:
+    print("this value is true")
+else:
+    print("this value is false")
+a = 5
+if a == 3:
+    print("this value is 3")
+elif a == 5:
+    print("this value is 5")
+else:
+    print("this value is not 3 or 5")
+
+# can profile A access profile B
+# a = isFriend
+# b = isBlocked
+# c = isAdmin
+# d = isMarkZuckerberg
+
+# a b c d q
+# 0 0 0 0 0
+# 0 0 0 1 1
+# 0 0 1 0 1
+# 0 0 1 1 1 
+# 0 1 0 0 0
+# 0 1 0 1 1
+# 0 1 1 0 0
+# 0 1 1 1 1
+# 1 0 0 0 1
+# 1 0 0 1 1
+# 1 0 1 0 1 
+# 1 0 1 1 1
+# 1 1 0 0 0
+# 1 1 0 1 1
+# 1 1 1 0 0
+# 1 1 1 1 1
+
+
+
+
+
+
+
+
+
+
+
+
+
